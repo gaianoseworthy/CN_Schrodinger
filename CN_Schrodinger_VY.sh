ef660738b6ad6de0908721ae2c39cc5a1b3c678b
gaianoseworthy <gaianoseworthy@gmail.com>
2024-06-18 12:26:37 -0300

    Removed outdated code

--- CN_Schrodinger_VY.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --time=24:00:00            # time in days-hours:minutes:seconds, COMP PREDICTED 10
-#SBATCH --cpus-per-task=16        # cpu-cores per task (>1 if multi-threaded tasks)
-#SBATCH --mem=32G                 # memory
-#SBATCH --mail-user=gaianoseworthy@gmail.com
-#SBATCH --mail-type=ALL
-module load python/3.10
-module load scipy-stack
-python -u "CN_Schrodinger_VY.py"
