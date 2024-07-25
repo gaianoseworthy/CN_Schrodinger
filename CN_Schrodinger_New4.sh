e03b1b9c358b4350a661a91273d5f763b096b36c
gaianoseworthy <gaianoseworthy@gmail.com>
2024-07-24 21:41:05 -0300

    Added the other 500 quantum results

---
+++ CN_Schrodinger_New4.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --time=1:15:00            # time in days-hours:minutes:seconds, COMP PREDICTED 10
+#SBATCH --cpus-per-task=8       # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --array=0-124
+#SBATCH --mail-user=gaianoseworthy@gmail.com
+#SBATCH --mail-type=ALL
+#SBATCH --mem=4G
+module load python/3.10
+module load scipy-stack
+python -u "CN_Schrodinger_New4.py" $SLURM_ARRAY_TASK_ID
